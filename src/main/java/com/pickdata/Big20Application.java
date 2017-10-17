19fe8ae4fb80df36b013fbda252dde91efaac53c
yearin <yearin92@gmail.com>
2017-10-17 18:11:05 +0900

    first commit

---
+++ src/main/java/com/pickdata/Big20Application.java
@@ -0,0 +1,12 @@
+package com.pickdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Big20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Big20Application.class, args);
+	}
+}
