eafa567311a4711e2f040da6ef6e2e3804e112c2
java@guestBig <kkolx2@nate.com>
2017-10-19 16:08:51 +0900

    score

--- src/main/java/com/pickdata/controller/PickdataContoller.java
+++ src/main/java/com/pickdata/controller/PickdataContoller.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.mysql.fabric.xmlrpc.base.Array;
 import com.pickdata.beta.Beta;
 import com.pickdata.columns.ColumnList;
 import com.pickdata.domain.CreditDO;
